716356373d767f010babc8bcbd31d634c1c4c516
Asami119 <iamas119@yahoo.co.jp>
2022-02-04 21:11:06 +0900

    バリデーション・エラーメッセージの表示を設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,29 +4,27 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # with_options presence: true do
-  #   validates :nickname
-  #   validates :last_name
-  #   validates :first_name
-  #   validates :last_name_kana
-  #   validates :first_name_kana
-  #   validates :birth_day
-  # end
+  with_options presence: true do
+    validates :nickname
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
+    validates :birth_day
+  end
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 
-  # validates :encrypted_password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i,
-  #   message: "半角英数字混合での入力が必須です" }
+  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,
+    message: "は、全角（漢字・ひらがな・カタカナ）で入力してください" } do
+    validates :last_name
+    validates :first_name
+  end
 
-  # with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,
-  #   message: "全角（漢字・ひらがな・カタカナ）での入力が必須です" } do
-  #   validates :last_name
-  #   validates :first_name
-  # end
-
-  # with_options format: { with: /\A[ぁ-んァ-ヶー]+\z/,
-  #   message: "全角（カタカナ）での入力が必須です" } do
-  #   validates :last_name
-  #   validates :first_name
-  # end
-
+  with_options format: { with: /\A[ァ-ヶー]+\z/,
+    message: "は、全角カタカナで入力してください" } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
 end
