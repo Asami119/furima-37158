ead9cada5a6c0f79060ec020278defa62d08a264
Asami119 <iamas119@yahoo.co.jp>
2022-02-06 14:41:02 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :postage_id, :prefecture_id, :days_post_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
