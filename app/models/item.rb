eae2139f8d499598e371271a728c5c48bbff47cb
Asami119 <iamas119@yahoo.co.jp>
2022-02-07 12:03:52 +0900

    ActiveHashでカテゴリーを実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :postage, :prefecture, :days_post
+
   belongs_to :user
   has_one_attached :image
 
@@ -13,4 +16,13 @@ class Item < ApplicationRecord
     validates :price
     validates :image
   end
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :condition_id
+    validates :postage_id
+    validates :prefecture_id
+    validates :days_post_id
+  end
+
 end
