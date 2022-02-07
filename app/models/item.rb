2710df4583696760b1a0df4655b4404a1cd4b7c8
Asami119 <iamas119@yahoo.co.jp>
2022-02-07 14:22:05 +0900

    価格のバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -25,4 +25,7 @@ class Item < ApplicationRecord
     validates :days_post_id
   end
 
+  validates :price, numericality: { only_integer: true, message: 'は半角数値のみ設定可能です' }
+  validates :price, numericality: { greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999, message: 'は、¥300~¥9,999,999の間で設定してください' }
+
 end
