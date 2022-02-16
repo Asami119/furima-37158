1feaafc7ed609d96d8bb7a759eaecabedda7c8b1
Asami119 <iamas119@yahoo.co.jp>
2022-02-16 13:53:50 +0900

    バリデーションを設定

--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -13,10 +13,7 @@ class PurchaseShipping
 
   validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
-  # validates :address_city
-  # validates :address_block
-  # validates :address_building
-  # validates :phone_number
+  validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "は、10桁以上11桁以内の半角数値のみ入力可能です"}
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
