c38b03864b6dff77e3937158b4de0859c91d15f3
Asami119 <iamas119@yahoo.co.jp>
2022-02-17 15:02:52 +0900

    「:item_id」の重複を解消

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -22,7 +22,7 @@ class PurchasesController < ApplicationController
 
   def purchase_params
     params.require(:purchase_shipping).permit(:postcode, :prefecture_id, :address_city, :address_block, :address_building,
-                                              :phone_number, :item_id).merge(
+                                              :phone_number).merge(
                                                 user_id: current_user.id, item_id: @item.id, token: params[:token]
                                               )
   end
