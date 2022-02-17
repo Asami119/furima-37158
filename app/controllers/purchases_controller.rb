edc9e36297f5ac17e31dfcd9e84bc813dd27c603
Asami119 <iamas119@yahoo.co.jp>
2022-02-17 14:35:29 +0900

    Rubocopを実行

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -6,7 +6,7 @@ class PurchasesController < ApplicationController
   def index
     @purchase_shipping = PurchaseShipping.new
   end
- 
+
   def create
     @purchase_shipping = PurchaseShipping.new(purchase_params)
     if @purchase_shipping.valid?
@@ -21,7 +21,10 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_shipping).permit(:postcode, :prefecture_id, :address_city, :address_block, :address_building, :phone_number, :item_id).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+    params.require(:purchase_shipping).permit(:postcode, :prefecture_id, :address_city, :address_block, :address_building,
+                                              :phone_number, :item_id).merge(
+                                                user_id: current_user.id, item_id: @item.id, token: params[:token]
+                                              )
   end
 
   def set_item
@@ -29,7 +32,7 @@ class PurchasesController < ApplicationController
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
       amount: @item.price,
       card: purchase_params[:token],
--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -1,6 +1,7 @@
 class PurchaseShipping
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :address_city, :address_block, :address_building, :phone_number, :token
+  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :address_city, :address_block, :address_building, :phone_number,
+                :token
 
   with_options presence: true do
     validates :user_id
@@ -12,12 +13,13 @@ class PurchaseShipping
     validates :token
   end
 
-  validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
-  validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "は、10桁以上11桁以内の半角数値のみ入力可能です"}
+  validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'は、10桁以上11桁以内の半角数値のみ入力可能です' }
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    Shipping.create(postcode: postcode, prefecture_id: prefecture_id, address_city: address_city, address_block: address_block, address_building: address_building, phone_number: phone_number, purchase_id: purchase.id)
+    Shipping.create(postcode: postcode, prefecture_id: prefecture_id, address_city: address_city, address_block: address_block,
+                    address_building: address_building, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
