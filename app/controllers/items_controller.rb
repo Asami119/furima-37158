4dec0b32cdd2dd4c1cf29d72f2b0ab1fd862e61f
Asami119 <iamas119@yahoo.co.jp>
2022-02-17 15:22:23 +0900

    任意修正（リファクタリング）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,9 +51,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    if current_user.id != @item.user_id
-      redirect_to root_path
-    elsif @item.purchase.blank? == false
+    if current_user.id != @item.user_id || @item.purchase.present?
       redirect_to root_path
     end
   end
